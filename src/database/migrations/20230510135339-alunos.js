1184b16ba14353bce08d4da199f91c62b7a3f5fe
Raphael Gomes <rafagomes1706@gmail.com>
2023-05-10 13:45:08 -0300

    feat: cria a primeira migration de alunos

---
+++ src/database/migrations/20230510135339-alunos.js
@@ -0,0 +1,55 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("alunos", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+
+      Weight: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+
+      height: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("alunos");
+  },
+};
