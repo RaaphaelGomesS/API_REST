150f6738286b6e4beb2673e4a696aae75317f4f2
Raphael Gomes <rafagomes1706@gmail.com>
2023-05-17 15:39:13 -0300

    feat: cria o CRUD de alunos

---
+++ src/controllers/AlunoController.js
@@ -0,0 +1,91 @@
+import Aluno from "../models/Aluno";
+
+class AlunoController {
+  async index(req, res) {
+    const alunos = await Aluno.findAll();
+    res.json(alunos);
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({ errors: ["Faltando ID"] });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({ errors: ["Aluno não existe."] });
+      }
+
+      return res.json(aluno);
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async create(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json({ msg: "Aluno criado!", aluno: aluno });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors?.map((err) => err.message) });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({ errors: ["Faltando ID"] });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({ errors: ["Aluno não existe."] });
+      }
+
+      const newAluno = await aluno.update(req.body);
+
+      return res.json({ msg: "Aluno atualizado!", aluno: newAluno });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({ errors: ["Faltando ID"] });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({ errors: ["Aluno não existe."] });
+      }
+
+      await aluno.destroy();
+      return res.json({ msg: "Aluno deletado!" });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+}
+
+export default new AlunoController();
