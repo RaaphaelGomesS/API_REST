336546f88a0443226f1b5e1447d06f753ef77064
Raphael Gomes <rafagomes1706@gmail.com>
2023-05-24 11:24:29 -0300

    feat: adiciona a necessidade de login nas rotas

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
+import loginRequired from "../middleware/loginRequired";
 
 const router = new Router();
 
 router.post("/", UserController.create);
-router.put("/", UserController.update);
-router.delete("/", UserController.delete);
+router.put("/", loginRequired, UserController.update);
+router.delete("/", loginRequired, UserController.delete);
 
 export default router;
