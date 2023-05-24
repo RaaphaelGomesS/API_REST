f16179a596e169dc414ecb7244de3e305615247e
Raphael Gomes <rafagomes1706@gmail.com>
2023-05-24 11:18:02 -0300

    feat: adciona o import da rota de photo e cria o local static das imagens

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
-import express from 'express';
-import home from './src/routes/home';
-import user from './src/routes/user';
+import express from "express";
+import { resolve } from "path";
+import home from "./src/routes/home";
+import user from "./src/routes/user";
 import token from "./src/routes/token";
 import aluno from "./src/routes/aluno";
+import photo from "./src/routes/photo";
 import "./src/database";
 
 dotenv.config();
@@ -18,13 +20,15 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
+    this.app.use(express.static(resolve(__dirname, "uploads")));
   }
 
   routes() {
-    this.app.use('/', home);
-    this.app.use('/users/', user);
-    this.app.use('/tokens/', token);
-    this.app.use('/alunos/', aluno);
+    this.app.use("/", home);
+    this.app.use("/users/", user);
+    this.app.use("/tokens/", token);
+    this.app.use("/alunos/", aluno);
+    this.app.use("/photo/", photo);
   }
 }
 
